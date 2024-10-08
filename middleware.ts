1c1b4e954581bc7765031dfbe8ec93ee91905ae6
Kumar Gaurav <gaurav.khatana@anarish.com>
2024-10-08 16:44:37 +0530

    middleware updates for production

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,27 @@ import { NextRequest, NextResponse } from 'next/server';
 // import { withClerkMiddlewar } from '@clerk/nextjs/server';
 import { authMiddleware } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"],
-});
+// export default authMiddleware({
+//   publicRoutes: ["/api/uploadthing"],
+// });
+
+export default async () => {
+  try {
+    return await authMiddleware({
+      publicRoutes: ["/api/uploadthing"],
+    });
+  } catch (error) {
+    console.error("Error in auth middleware:", error);
+    return NextResponse.error();
+  }
+};
+
+
 
 export const config = {
   matcher: [
     '/((?!.*\\..*|_next).*)', // Exclude static files and Next.js internals
-    '/',
-    '/(api|trpc)(.*)', // Include API routes
+    // '/',
+    // '/(api|trpc)(.*)', // Include API routes
   ],
 };
